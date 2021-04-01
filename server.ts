b828cc5b2c90c492dede1773a77db1f6d8b6b0e0
Maciej Bojko <maciej.bojko@gmail.com>
2021-04-01 23:12:37 +0200

    Try for deploy attempt #1

---
+++ server.ts
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+function requireHTTPS(req, res, next): void {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+
+  next();
+}
+
+const app = express();
+app.use(requireHTTPS);
+
+const pathToDist = path.join(__dirname, 'dist', 'heroku1');
+
+app.use(express.static(pathToDist));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', {root: pathToDist});
+});
+
+const port = process.env.PORT || 8080;
+
+console.log(`Listening on port ${port}`);
+
+app.listen(port);
