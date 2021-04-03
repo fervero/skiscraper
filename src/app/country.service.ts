07a20eb78f5f7c7d6b26e01053b5c079d96ef846
Maciej Bojko <maciej.bojko@gmail.com>
2021-04-03 20:23:25 +0200

    Mock data for now

--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Observable, of } from 'rxjs';
+import { mockCountries } from './mock-data/countries';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,8 @@ export class CountryService {
   private readonly url = 'countries';
   constructor(private httpClient: HttpClient) {}
 
-  getAll = () =>
-    this.httpClient.get<string[]>(`${environment.apiUrl}/${this.url}`);
+  getAll = (): Observable<string[]> =>
+    environment.production
+      ? of(mockCountries)
+      : this.httpClient.get<string[]>(`${environment.apiUrl}/${this.url}`);
 }
--- src/app/resort.service.ts
+++ src/app/resort.service.ts
@@ -9,7 +9,7 @@ import {
 } from './interfaces/models';
 
 import { ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import {
   APIResort,
   APIResorts,
@@ -19,6 +19,9 @@ import {
 
 import { map } from 'rxjs/operators';
 import { environment } from '../environments/environment';
+import { mockResorts } from './mock-data/resorts';
+
+console.log(mockResorts);
 
 @Injectable({
   providedIn: 'root',
@@ -31,6 +34,43 @@ export class ResortService {
   constructor(private httpClient: HttpClient) {}
 
   getFromParams = (
+    { size, page }: APIPaginationParams,
+    filterParams: APIFilterParams = {},
+    sortOptions: APISortParams = null
+  ): Observable<ResortsPaginatedResponse> =>
+    environment.production
+      ? this.getMockData()
+      : this.getData({ size, page }, filterParams, sortOptions);
+
+  getFromParamMap = (
+    paramMap: ParamMap
+  ): Observable<ResortsPaginatedResponse> => {
+    const size = +paramMap.get('size') || this.DEFAULT_PAGE_SIZE;
+    const page = +paramMap.get('page') || this.DEFAULT_PAGE;
+    const country = paramMap.getAll('country');
+    const region = paramMap.get('region');
+    const sort = paramMap.get('sort');
+
+    return this.getFromParams(
+      { page, size },
+      { country: country.join(','), region },
+      sort ? { sort } : null
+    );
+  };
+
+  private getMockData = (): Observable<ResortsPaginatedResponse> =>
+    of(mockResorts).pipe(
+      map((res) => ({ ...res, resorts: res.resorts.map(this.parseResort) }))
+    );
+
+  private parseResort = (resort: APIResort): Resort => ({
+    ...resort,
+    breadcrumbs1: this.parseBreadcrumb(resort.breadcrumbs1),
+    breadcrumbs2: null,
+    breadcrumbs3: null,
+  });
+
+  private getData = (
     { size, page }: APIPaginationParams,
     filterParams: APIFilterParams = {},
     sortOptions: APISortParams = null
@@ -56,29 +96,6 @@ export class ResortService {
         map((res) => ({ ...res, resorts: res.resorts.map(this.parseResort) }))
       );
   };
-
-  getFromParamMap = (
-    paramMap: ParamMap
-  ): Observable<ResortsPaginatedResponse> => {
-    const size = +paramMap.get('size') || this.DEFAULT_PAGE_SIZE;
-    const page = +paramMap.get('page') || this.DEFAULT_PAGE;
-    const country = paramMap.getAll('country');
-    const region = paramMap.get('region');
-    const sort = paramMap.get('sort');
-
-    return this.getFromParams(
-      { page, size },
-      { country: country.join(','), region },
-      sort ? { sort } : null
-    );
-  };
-
-  private parseResort = (resort: APIResort): Resort => ({
-    ...resort,
-    breadcrumbs1: this.parseBreadcrumb(resort.breadcrumbs1),
-    breadcrumbs2: null,
-    breadcrumbs3: null,
-  });
 
   private parseBreadcrumb = (breadcrumbJSON: string): Breadcrumb[] =>
     (JSON.parse(breadcrumbJSON) as Breadcrumb[]).slice(0, 3);
