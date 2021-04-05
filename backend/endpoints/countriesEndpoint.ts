99cf4b061bf54abd65e8b53a90a55bdf737c616d
Maciej Bojko <maciej.bojko@gmail.com>
2021-04-05 21:15:13 +0200

    Typing in selectDistinct

--- backend/endpoints/countriesEndpoint.ts
+++ backend/endpoints/countriesEndpoint.ts
@@ -1,21 +1,25 @@
 import { Request, Response } from 'express';
 import mockCountries from '../mock-data/mock-countries';
 import { Resort } from '../sql-driver';
+import {Model} from 'sequelize';
 
 const heroku = process.env.ENVIRONMENT === 'heroku';
+
+type Constructor<T> = new (...args: any[]) => T;
+
+type ModelType<T extends Model<T>> = Constructor<T> & typeof Model;
 
 const staticCountries = (req: Request, res: Response) =>
   res.send(mockCountries);
 
-// FIXME don't use `any`
-const selectDistinct = (model: any, field: string) =>
+const selectDistinct = (model: ModelType<any>, field: string): Promise<string[]> =>
   model
     .aggregate(field, 'DISTINCT', { plain: false })
     .then((response: any) =>
       (response as any[]).map(({ DISTINCT }) => DISTINCT)
     );
 
-const dbCountries = (req: Request, res: Response) => {
+const dbCountries = (req: Request, res: Response) =>
   selectDistinct(Resort, 'country')
     .then((result: string[]) => res.send(result))
     .catch((err: any) => {
@@ -23,6 +27,5 @@ const dbCountries = (req: Request, res: Response) => {
         message: err.message || 'Some error occurred while retrieving resorts.',
       });
     });
-};
 
 export const countriesEndpoint = dbCountries;
